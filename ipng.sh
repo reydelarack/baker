c5180ff34253c4292e3c6ec006061de3302e9f5b
Teran McKinney <teran.mckinney@rackspace.com>
2014-05-16 15:30:05 +0000

    Sleep 1 second between uuid to ip requests

--- ipng.sh
+++ ipng.sh
@@ -5,6 +5,7 @@ UUID=$2
 BYPASS=$3
 
 while [ -z "$IP" ]; do
+	sleep 1
         IP="`supernova $ACCOUNT $BYPASS show --minimal $UUID | grep accessIPv4 | head -n 1 | awk '{print $4}' | tr -d '|'`"
 done
 
