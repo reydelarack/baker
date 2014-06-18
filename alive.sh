559948e2c056720f1950eac370e59063464aeadb
Teran McKinney <teran.mckinney@rackspace.com>
2014-06-18 17:55:14 +0000

    Removed ICMP check
    Apparently, ping -t on Linux is generally TTL, -t on FreeBSD is
    timeout. Lowered SSH connect timeout. SSH is the only alive
    indicator now.

--- alive.sh
+++ alive.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 IP=$1
-SSHARGS='-q -oConnectTimeout=30 -oCheckHostIP=no -oStrictHostKeyChecking=no -oIdentitiesOnly=yes -oUserKnownHostsFile=/dev/null -oBatchMode=yes -o VerifyHostKeyDNS=no'
+SSHARGS='-q -oConnectTimeout=10 -oCheckHostIP=no -oStrictHostKeyChecking=no -oIdentitiesOnly=yes -oUserKnownHostsFile=/dev/null -oBatchMode=yes -o VerifyHostKeyDNS=no'
 
 [ -z "$IP" ] && exit 1
 
-(while ! ping -t 1 -n -c 1 "$IP"; do
-        continue
-done
-
-while ! ssh $SSHARGS "root@$IP" true; do
+(while ! ssh $SSHARGS "root@$IP" true; do
 	sleep 1
 	continue
 done) &> /dev/null
